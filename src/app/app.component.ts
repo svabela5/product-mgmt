9e961fb7918c413738649da288d758809f7ad0c5
svabela5 <sven.abela36@gmail.com>
2024-12-19 13:00:02 +0100

    Implemented routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { ProductListComponent } from "./product-list/product-list.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ProductListComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "products", component: ProductListComponent},
+    {path: "products/:id", component: ProductDetailComponent},
+    {path: "home", component:HomeComponent},
+    {path: "**", redirectTo: "/home", pathMatch:"full"}
+];
