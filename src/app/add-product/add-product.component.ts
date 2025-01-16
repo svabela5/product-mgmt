88a9c137ce74ebfabc0d06b22622c2d003727142
svabela5 <sven.abela36@gmail.com>
2025-01-16 12:54:24 +0100

    partially implemented form validation

--- src/app/add-product/add-product.component.ts
+++ src/app/add-product/add-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ProductService } from '../services/product.service';
 import { ProductAddUpdate } from '../dto/product-add-update.dto';
 import { Product } from '../dto/product.dto';
@@ -21,13 +21,13 @@ export class AddProductComponent implements OnInit {
 
   ngOnInit(): void {
     this.productForm = this.formBuilder.group({
-      code: null,
-      name: null,
-      description: null,
-      releaseDate: null,
-      price: null,
-      starRating: null,
-      imageUrl: null
+      code: ['', Validators.required],
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      releaseDate: ['', Validators.required],
+      price: ['', Validators.required],
+      starRating: ['', Validators.required],
+      imageUrl: ['', Validators.required]
     })
   }
 
@@ -39,5 +39,10 @@ export class AddProductComponent implements OnInit {
       console.log(JSON.stringify(addedProduct));
       this.router.navigate(["/products"]);
     })
+  }
+
+  shouldProcessControlValidationMessages(controlName: string){
+    let control = this.productForm.get(controlName)
+    return ((control!.touched || control!.dirty) && control!.errors);
   }
 }
